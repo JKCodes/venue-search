2f4874483d0a8492616e82704c5113c6fe3ea151
Joe Kim <josephkim0224@gmail.com>
2017-10-21 12:50:28 -0400

    remove console log from venuesReducer

--- src/reducers/venuesReducer.js
+++ src/reducers/venuesReducer.js
@@ -9,7 +9,6 @@ export default (state = initialState, action) => {
   switch (action.type) {
 
     case constants.VENUES_RECEIVED:
-      console.log('VENUES_RECEIVED: ' + JSON.stringify(action.venues))
       let updated = Object.assign({}, state)
       updated['venues'] = action.venues
       return updated
