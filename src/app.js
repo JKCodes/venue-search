87744ab7d6d2327f0b752ca070cff407b3ce04e8
Joe Kim <josephkim0224@gmail.com>
2017-10-21 11:08:11 -0400

    create Nav component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Nav } from './components'
 
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Nav />
         React App
       </div>
     )
