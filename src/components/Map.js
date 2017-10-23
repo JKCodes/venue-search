ab12fde2e65c5bce8192e3e6749611fbf01b61bb
Joe Kim <josephkim0224@gmail.com>
2017-10-23 18:11:46 -0400

    Map component will now update on search to include markers with numbers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { connect } from 'react-redux'
 
 class Map extends Component {
   
@@ -21,13 +22,20 @@ class Map extends Component {
 
   render() {
 
-    const markers = this.props.markers || []
+    const venues = this.props.venues || []
+    const markers = venues.map((venue,i) => {
+      return { key: i, position: {lat: venue["location"]["lat"], lng: venue["location"]["lng"]}, label: `${i}`}
+    })
+    const center = markers.length > 0 ? markers[0].position : {lat:0 , lng: 0}
+
+    console.log(center)
 
     return (
       <GoogleMap
         ref={this.mapLoaded.bind(this)}
         defaultZoom={this.props.zoom}
-        defaultCenter={{lat:40.728199, lng:-73.9894738}}>
+        defaultCenter={center}
+        center={center}>
         {markers.map((marker, index) => (
             <Marker {...marker} />
           )
@@ -37,4 +45,10 @@ class Map extends Component {
   }
 }
 
-export default withGoogleMap(Map)
+const mapStateToProps = (state) => {
+  return {
+    venues: state.venue.venues
+  }
+}
+
+export default connect(mapStateToProps)(withGoogleMap(Map))
