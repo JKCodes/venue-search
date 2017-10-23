28c2af5143415585d807915bc92b98fd47a6a3a7
Joe Kim <josephkim0224@gmail.com>
2017-10-23 18:10:36 -0400

    fixed the position of the search bar

--- src/app.js
+++ src/app.js
@@ -6,24 +6,24 @@ import { Provider } from 'react-redux'
 
 const app = (
   <Provider store={store.initialize()}>
-        <div>
-          <Nav />
           <div className="container">
+            <div className="row" style={{position: 'fixed', left:0, top:0, width: '100%', zIndex: '2'}}>
+              <Nav />
+            </div>
             <div className="row">
               <div className="col-sm-6" style={{position: 'fixed', left:0, top: 80}}>
                 <Map
-                  zoom={14}
+                  zoom={12}
                   containerElement={<div style={{height: '89vh'}} /> }
                   mapElement={<div style={{height:'89vh'}} /> }
                 />
               </div>
 
-              <div className="col-sm-6 col-sm-offset-6">
+              <div className="col-sm-6 col-sm-offset-6" style={{marginTop: 80}}>
                 <Venues />
               </div>
             </div>
           </div>
-        </div>
   </Provider>
 )
 
